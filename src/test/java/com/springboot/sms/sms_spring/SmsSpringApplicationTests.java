b08380768ab937c2a33c2eaa21b0ef3bd8144198
joyjitdas3918 <138395405+joyjitdas3918@users.noreply.github.com>
2025-03-08 14:17:36 +0530

    connect db

---
+++ src/test/java/com/springboot/sms/sms_spring/SmsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.sms.sms_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
